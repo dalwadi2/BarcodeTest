e92bbbecdcb5436a0e57818d8807802d1e9c65bf
Harsh Dalwadi <dalwadi2@gmail.com>
2016-02-19 19:08:58 +0530

    Code with LOcation Commit

--- app/src/main/java/com/example/barcodetest/AndroidBarcodeTestActivite.java
+++ app/src/main/java/com/example/barcodetest/AndroidBarcodeTestActivite.java
@@ -2,15 +2,21 @@ package com.example.barcodetest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class AndroidBarcodeTestActivite extends Activity {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
+    ViewFlipper mview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.barcode);
+
+        mview = (ViewFlipper) findViewById(R.id.mybarcodeview);
         AndroidBarcodeView view = new AndroidBarcodeView(this);
-        
-        setContentView(view);
+        mview.addView(view);
     }
 }
